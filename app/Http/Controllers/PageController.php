b7106cf70cc10062ab3c8f249394b66ec834fffb
Andrea <andrewbons@gmail.com>
2017-12-15 17:11:28 +0100

    messaggio di prova

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function home(Request $request){
+        //prova
         return view("frontend.home.index");
     }
 }
